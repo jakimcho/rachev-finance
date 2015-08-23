f91ac861c9dacb7bbf7a23a983eca653446e5e3b
jakimcho <jakimcho@gmail.com>
2015-08-23 23:27:30 +0300

    Create User Conteoller and added new users resource in the routes config

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+
+  def index
+    @users = User.all
+  end
+
+  def create
+  end
+
+  def new
+    
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   #   resources :products
 
   resources :accounts
+  resources :users
   # Example resource route with options:
   #   resources :products do
   #     member do
