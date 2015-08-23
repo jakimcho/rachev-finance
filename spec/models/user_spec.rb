b7b1cfd297ce13bbce109adb3a55d3ae19d5246d
jakimcho <jakimcho@gmail.com>
2015-08-23 20:33:15 +0300

    specs for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@
 require 'rails_helper'
 
 describe User do
-  before :all do
+  before :each do
            @consts = {
                        user1_name: "Yakim",
                        user2_name: "Pesho",
@@ -49,6 +49,46 @@ describe User do
  
   it "should create a new instance given valid attributes" do
     User.create(@user2_attr)
+  end
+
+  it "should require name" do
+    no_name_user = User.new(@user2_attr.merge(name: ""))
+    
+    expect(no_name_user).to_not be_valid
+  end
+
+  it "should require password" do
+    no_password_user = User.new(@user2_attr.merge(password: ""))
+    
+    expect(no_password_user).to_not be_valid
+  end
+
+  it "should require email" do
+    no_email_user = User.new(@user2_attr.merge(email: ""))
+    
+    expect(no_email_user).to_not be_valid
+  end
+
+  it "should reject email duplication" do
+    User.create!(@user2_attr)
+    existed_email_user = User.new(@user2_attr.merge(name: "Pesho123"))
+
+    expect(existed_email_user).to_not be_valid
+  end
+
+  it "should reject email duplication case insensitive" do
+    User.create!(@user2_attr)
+    up_case_email = @user2_attr[:email].upcase
+
+    existed_email_user = User.new(@user2_attr.merge(email: up_case_email))
+
+    expect(existed_email_user).to_not be_valid
+  end
+
+  it "should require family_id" do
+    no_family_user = User.new(@user2_attr.merge(family_id: ""))
+    
+    expect(no_family_user).to_not be_valid
   end
 
   it "should has correct parameters" do
