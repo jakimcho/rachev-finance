9d8a587fdc06ac038e253e7747b098df6a9a5400
jakimcho <jakimcho@gmail.com>
2015-09-12 11:31:52 +0300

    fix account migration to drop table if exists

--- db/migrate/20150903205614_create_accounts.rb
+++ db/migrate/20150903205614_create_accounts.rb
@@ -1,5 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
+  	drop_table(:accounts, if_exists: true)
+  	
     create_table :accounts do |t|
       t.string :name
       t.decimal :balance
