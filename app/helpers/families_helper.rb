a6adf2a9b6c9c0f69716b02a38b5c7437885d313
jakimcho <jakimcho@gmail.com>
2016-04-03 18:10:26 +0300

    Added table representation for /users and /users:index/accounts

--- app/helpers/families_helper.rb
+++ app/helpers/families_helper.rb
@@ -1,2 +1,10 @@
 module FamiliesHelper
+  def family_accounts family
+    family = Family.find(family)
+    family_accounts = []
+
+    family.users.each do |u|
+      family_accounts += u.accounts
+    end
+  end
 end
